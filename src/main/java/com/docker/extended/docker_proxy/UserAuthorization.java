4d9555d31143bef9a8a324d30767e6d0890fddcd
Eduard Glekler <eduard.glekler@yandex.ru>
2020-12-25 07:00:25 +0300

    test12

--- src/main/java/com/docker/extended/docker_proxy/UserAuthorization.java
+++ src/main/java/com/docker/extended/docker_proxy/UserAuthorization.java
@@ -31,7 +31,7 @@ public class UserAuthorization {
         log.info("OK2.2");
         log.info(userEndpoints.toString());
         log.info("OK.2.2.1");
-        List<MEndpoint> matchesEndpoints = userEndpoints.stream().filter(ep -> Pattern.matches(endpoint, ep.getContent())).collect(Collectors.toList());
+        List<MEndpoint> matchesEndpoints = userEndpoints.stream().filter(ep -> endpoint.equals(ep.getContent()) || Pattern.matches(endpoint, ep.getContent())).collect(Collectors.toList());
         log.info("OK2.2.2");
         log.info(matchesEndpoints.toString());
         if(matchesEndpoints.isEmpty()) {log.info("OK2.2.3"); return false;}
